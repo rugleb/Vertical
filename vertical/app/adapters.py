3a4d854b938ae4d4291031c6948457a25651d6c4
Gleb Karpushkin <rugleb@gmail.com>
2020-04-18 21:34:22 +0300

    created HTTP Adapters

---
+++ vertical/app/adapters.py
@@ -0,0 +1,54 @@
+from starlette.requests import Request
+from starlette.responses import Response
+
+from vertical import hdrs
+
+from .protocols import RequestProtocol, ResponseProtocol
+
+__all__ = ("RequestAdapter", "ResponseAdapter")
+
+
+class RequestAdapter(RequestProtocol):
+
+    __slots__ = (
+        "identifier",
+        "method",
+        "path",
+        "body",
+        "remote_addr",
+        "referer",
+        "user_agent",
+        "authorization",
+    )
+
+    def __init__(self, request: Request):
+        self.identifier = request.headers.get(hdrs.X_REQUEST_ID)
+
+        self.method = request.method
+        self.path = request.url.path
+        self.body = request.state.body
+
+        self.referer = request.headers.get(hdrs.REFERER)
+
+        host, port = request.client
+        self.remote_addr = f"{host}:{port}"
+
+        user_agent = request.headers.get(hdrs.USER_AGENT, "")
+        self.user_agent = user_agent[:64]
+
+        self.authorization = request.headers.get(hdrs.AUTHORIZATION)
+
+
+class ResponseAdapter(ResponseProtocol):
+
+    __slots__ = (
+        "request",
+        "body",
+        "code",
+    )
+
+    def __init__(self, request: RequestAdapter, response: Response):
+        self.request = request
+
+        self.body = response.body.decode("utf-8")
+        self.code = response.status_code
