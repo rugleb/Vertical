4a171a7d068efbda5e36c2f78bcaa4848c8d0cc4
Gleb Karpushkin <rugleb@gmail.com>
2020-04-19 20:38:52 +0300

    created entrypoint script

---
+++ main.py
@@ -0,0 +1,45 @@
+import uvicorn
+from environs import Env
+
+import vertical
+
+env = Env()
+
+config = {
+    "auth_service": {
+        "pool": {
+            "dsn": env.str("AUTH_DB_URL"),
+            "min_size": env.int("AUTH_DB_MIN_SIZE", 0),
+            "max_size": env.int("AUTH_DB_MAX_SIZE", 5),
+            "max_queries": env.int("AUTH_DB_MAX_QUERIES", 1000),
+            "timeout": env.float("AUTH_DB_TIMEOUT", 10),
+            "command_timeout": env.float("AUTH_DB_COMMAND_TIMEOUT", 5),
+        },
+    },
+    "hunter_db": {
+        "bind": {
+            "name_or_url": env.str("HUNTER_DB_URL"),
+            "echo": env.bool("HUNTER_DB_ECHO", False),
+            "echo_pool": env.bool("HUNTER_DB_ECHO_POOL", False),
+            "encoding": env.str("HUNTER_DB_ENCODING", "utf-8"),
+            "isolation_level": env.str("HUNTER_DB_ISOLATION_LEVEL", None),
+            "max_overflow": env.int("HUNTER_DB_MAX_OVERFLOW", 5),
+            "pool_pre_ping": env.bool("HUNTER_DB_POOL_PRE_PING", False),
+            "pool_size": env.int("HUNTER_DB_POOL_SIZE", 5),
+            "pool_recycle": env.int("HUNTER_DB_POOL_RECYCLE", 3600),
+            "pool_timeout": env.int("HUNTER_DB_POOL_TIMEOUT", 10),
+            "strategy": env.str("HUNTER_DB_STRATEGY", "plain"),
+        },
+    },
+    "phone_service": {
+        "delta": env.int("PHONE_SERVICE_DELTA_DAYS", 180),
+    },
+}
+
+app = vertical.create_app(config)
+
+if __name__ == "__main__":
+    host = env.str("HOST", "127.0.0.1")
+    port = env.str("PORT", 8080)
+
+    uvicorn.run(app, host=host, port=port)
