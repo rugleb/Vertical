ccd2f11ca7f747244ebfd87bc2129bae9a433e05
gkarpushkin <rugleb@gmail.com>
2020-05-11 15:35:13 +0300

    configured asyncio from app factory

--- vertical/app/factory.py
+++ vertical/app/factory.py
@@ -1,13 +1,16 @@
+import asyncio
+from concurrent.futures.thread import ThreadPoolExecutor
 from enum import Enum
-from typing import TypedDict
+from typing import Dict, TypedDict
 
+import uvloop
 from starlette.applications import Starlette
 
 from .auth import AuthService, AuthServiceConfig
 from .endpoints import add_routes
 from .exception_handlers import add_exception_handlers
 from .hunter import HunterService, HunterServiceConfig
-from .log import setup_logging
+from .log import app_logger, setup_logging
 from .middlewares import add_middlewares
 
 __all__ = ("create_app", "AppConfig")
@@ -39,8 +42,24 @@ def setup_hunter_service(app: Starlette, config: HunterServiceConfig) -> None:
     app.add_event_handler(Signal.SHUTDOWN, hunter_service.cleanup)
 
 
+def setup_asyncio() -> None:
+    uvloop.install()
+
+    loop = asyncio.get_event_loop()
+
+    executor = ThreadPoolExecutor(thread_name_prefix="vertical")
+    loop.set_default_executor(executor)
+
+    def handler(_, context: Dict) -> None:
+        message = "Caught asyncio exception: {message}".format_map(context)
+        app_logger.warning(message)
+
+    loop.set_exception_handler(handler)
+
+
 def create_app(config: AppConfig) -> Starlette:
     setup_logging()
+    setup_asyncio()
 
     app = Starlette(debug=False)
 
